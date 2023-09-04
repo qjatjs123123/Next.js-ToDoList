125bc67c79e721e0ed0a9c9235ba5e6c34a3fdf0
Hong Boem Sun <qjatjs123123@naver.com>
2023-09-04 23:07:35 +0900

    todolist css구현

--- todolist/app/calender/TodoList.js
+++ todolist/app/calender/TodoList.js
@@ -1,53 +1,103 @@
 'use client'
 
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function TodoList(props) {
     const [divs, setDivs] = useState([]);
+    const divRefs = useRef([]);
     const timeList = () => {
         
         let arr = [];
         for (let i = 8; i < 25; i++) {
+            let tmp = '';
+            if ( i <= 12) tmp = `오전 ${i}시`;
+            else tmp = `오후 ${i-12}시`;
+            const tp = (i-8)*100 + 50;
             arr.push(
-                <div key={i} className="bordered-div">
-                    {i}
-                </div>
+                <span style={{position:'absolute', top:`${tp-12}px`, textAlign:'right',width:'100%'}} key={i}>{tmp}</span>
+            )
+        }
+        return arr
+    }
+
+    const hrDraw = () =>{
+        let arr = [];
+        for (let i = 8; i < 25; i++) {
+            const tp = (i-8)*100 + 50;
+            arr.push(
+                <hr onClick={() => alert("qw")} key={i} style={{position:'absolute', top:`${tp}px`, left:'0', margin:'0', borderTop: "1px solid black", width: '100%'}}>
+                </hr>
             )
         }
         return arr
     }
 
     const handleDivClick = (e) => {
-        const { pageX, pageY } = e;
+        console.log(e.nativeEvent.offsetX,e.nativeEvent.offsetY,e.target.tagName)
+        let left = e.nativeEvent.offsetX + 'px';
+        let top = e.target.scrollTop + e.nativeEvent.offsetY + 'px';
+        if(e.target.tagName === 'HR') top = e.target.scrollTop + parseInt(e.target.style.top) + 'px';
+        
+        console.log(top);
         const newDiv = {
-            left: pageX + 'px',
-            top: pageY + 'px',
+            left:  left,
+            top:  top
         };
-
+        divRefs.current.push(React.createRef());
         setDivs([...divs, newDiv]);
     }
     const handleDivsClick = (e) => {
         e.stopPropagation();
     }
     return (
-        <div className="todolist" onClick={handleDivClick}>
-            {timeList()}
-            {divs.map((div, index) => (
-                <div
-                    key={index}
-                    onClick={handleDivsClick}
-                    style={{
-                        position: 'absolute',
-                        left: div.left,
-                        top: div.top,
-                        width: '50px',
-                        height: '50px',
-                        background: 'lightgreen',
-                        resize:'both',
-                        overflow: 'auto'
-                    }}
-                ></div>
-            ))}
+        <div className="todolist" style={{display:"flex", flexDirection:'row'}}>
+            <div className="timelist">
+                {timeList()}
+            </div>
+            <div className="wall" onClick={handleDivsClick}></div>
+            
+            <div className="divlist"  onClick={handleDivClick}>
+                {hrDraw()}    
+                {divs.map((div, index) => (
+                    <div
+                        key={index}
+                        onClick={handleDivsClick}
+                        ref={divRefs.current[index]}
+                        style={{
+                            position: 'absolute',
+                            left: div.left,
+                            top: div.top,
+                            width: '200px',
+                            height: '100px',
+                            background: 'lightgreen',
+                            resize:'both',
+                            overflow: 'auto'
+                        }}
+                    ></div>
+                ))}
+            </div>
         </div>
+        
+        // <div className="todolist" onClick={handleDivClick} style={{ position: 'relative' }}>
+        //     <hr/>
+        //     {timeList()}
+        //     {divs.map((div, index) => (
+        //         <div
+        //             key={index}
+        //             onClick={handleDivsClick}
+        //             ref={divRefs.current[index]}
+        //             style={{
+        //                 position: 'absolute',
+        //                 left: div.left,
+        //                 top: div.top,
+        //                 width: '50px',
+        //                 height: '50px',
+        //                 background: 'lightgreen',
+        //                 resize:'both',
+        //                 overflow: 'auto'
+        //             }}
+        //         ></div>
+        //     ))}
+        // </div>
     )
 }
